c4f0949748c3558e3fea93ddeab2e7fcbcf1feeb
Charlie Chao <charliechao@outlook.com>
2018-04-02 23:05:01 -0700

    Reference Roboto fonts.

--- private/header.php
+++ private/header.php
@@ -4,7 +4,8 @@
   <meta charset="utf-8">
   <title><?php echo $page_title ?? 'PADb — Public Art Database'; ?></title>
   <meta name="viewport" content="width=device-width,initial-scale=1">
-  <link rel="stylesheet" href="css/styles.css">
+  <link href="https://fonts.googleapis.com/css?family=Roboto+Mono:700|Roboto:400,700" rel="stylesheet">
+  <link href="css/styles.css" rel="stylesheet">
 </head>
 <body>
   <main class="l-wrap-all">
