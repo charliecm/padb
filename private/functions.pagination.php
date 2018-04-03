6c3edf8517dade7f937ac2fac3c1dbfe761f1ba8
Charlie Chao <charliechao@outlook.com>
2018-04-03 16:44:02 -0700

    Improve pagination with ellipses.

--- private/functions.pagination.php
+++ private/functions.pagination.php
@@ -4,6 +4,9 @@
  */
 
 function show_pagination($page, $total, $params = array()) {
+  $range = 2;
+  $start = max($page - $range, 1);
+  $end = min($page + $range + 1, $total);
   if ($total < 1) return;
   if ($page > 1) {
     $params['page'] = $page - 1;
@@ -26,7 +29,21 @@ function show_pagination($page, $total, $params = array()) {
     <a <?php echo empty($prev) ? 'disabled' : "href=\"$prev\""; ?> class="btn btn--small">&lsaquo;</a>
   </li>
   <?php
-    for ($i = 1; $i < ($total + 1); $i++):
+    if ($start > 1):
+      $params['page'] = 1;
+      $url = get_url_params($params);
+  ?>
+  <li>
+    <a href="<?php echo $url; ?>" class="btn btn--small">1</a>
+  </li>
+  <?php if ($start > 2): ?>
+  <li>
+    ...
+  </li>
+  <?php endif; ?>
+  <?php endif; ?>
+  <?php
+    for ($i = $start; $i < $end; $i++):
       $params['page'] = $i;
       $url = get_url_params($params);
   ?>
@@ -34,6 +51,20 @@ function show_pagination($page, $total, $params = array()) {
     <a href="<?php echo $url; ?>" class="btn btn--small<?php echo ($i === $page) ? ' -active' : ''; ?>"><?php echo $i; ?></a>
   </li>
   <?php endfor; ?>
+  <?php
+    if ($end <= $total):
+      $params['page'] = $total;
+      $url = get_url_params($params);
+  ?>
+  <?php if ($end < $total): ?>
+  <li>
+    ...
+  </li>
+  <?php endif; ?>
+  <li>
+    <a href="<?php echo $url; ?>" class="btn btn--small"><?php echo $total; ?></a>
+  </li>
+  <?php endif; ?>
   <li>
     <a <?php echo empty($next) ? 'disabled' : "href=\"$next\""; ?> class="btn btn--small">&rsaquo;</a>
   </li>
